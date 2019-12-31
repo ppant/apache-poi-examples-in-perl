c2970b0de65cfa43ca9119de1fbaf9724a70ff10
Pradeep K. Pant <pantpradeep@gmail.com>
2019-12-31 13:34:06 +0530

    Fully working code of calling script for doc extension

--- push_custom_info.pl
+++ push_custom_info.pl
@@ -1,22 +1,17 @@
 #!/usr/local/bin/perl    
+	use POI;
 	# Call to push custom info in word documents	
-	# Handle DOC and SLS extesnions 
-	
-    if (defined($content_type) && ($content_type eq "application/x-msword" || $content_type eq "application/vnd.ms-excel" || $content_type eq "application/vnd.ms-powerpoint") ) {
-	my $path = $POI_temp_path."poi_view_tmp.doc";
-	my $buffer_temp;
-	open (MSHANDLE , ">".$path) or die "Can't open: $!\n";
-	print MSHANDLE $buffer;
-	close(MSHANDLE);
+	# Handle DOC, XLS  and PPT extension 
+	# Todo
+	#my $content_type;
+    #if (defined($content_type) && ($content_type eq "application/x-msword" || $content_type eq "application/vnd.ms-excel" || $content_type eq "application/vnd.ms-powerpoint") ) {
+	# Make sure to give full path of the doc file
+	my $path = "doc_test_file.doc";
+	# Make a POI object
 	my $alu = POI->new();
-	my $docname = $alu->PushCustomProperties($path,"Test_doc_file","ppant","2.0", now());	
-	open (MSHANDLE , $path) or die "Can't open: $!\n";
-	binmode(MSHANDLE);
-	read(MSHANDLE, $buffer_temp, -s $path);
-	close(MSHANDLE);
-	$buffer = $buffer_temp;
-	unlink($path);
-	}
+	# Time to call push properties routine which is inside POI.pm and written in Java
+	my $docname = $alu->PushProperties($path,"Test_doc_file","Test file doc type extension","2.0","ppant");	
+	
 	
 	
 	
