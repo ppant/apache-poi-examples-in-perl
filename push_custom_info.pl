3b25767ba717614739adbac4730ef3f0a70453b0
Pradeep K. Pant <pantpradeep@gmail.com>
2020-01-17 13:51:38 +0530

    Code refractor

--- push_custom_info.pl
+++ push_custom_info.pl
@@ -1,16 +1,14 @@
 #!/usr/local/bin/perl    
 	use POI;
 	# Call to push custom info in word documents	
-	# Handle DOC, XLS  and PPT extension 
-	# Todo
-	#my $content_type;
-    #if (defined($content_type) && ($content_type eq "application/x-msword" || $content_type eq "application/vnd.ms-excel" || $content_type eq "application/vnd.ms-powerpoint") ) {
+	#!/usr/local/bin/perl    
+	use POI;	
 	# Make sure to give full path of the doc file
 	my $path = "doc_test_file.doc";
 	# Make a POI object
-	my $alu = POI->new();
-	# Time to call push properties routine which is inside POI.pm and written in Java
-	my $docname = $alu->PushProperties($path,"Test_doc_file","Test file doc type extension","2.0","ppant");	
+	my $poi_obj = POI->new();
+	# Call push properties routine which is inside POI.pm and written in Java
+	my $docname = $poi_obj->PushProperties($path,"Test_doc_file","Test file doc type extension","2.0","ppant");
 	
 	
 	
